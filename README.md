7e9d6a53f1851170576a6970bca4c051f5590205
sakuH <saku.huuha@gmail.com>
2019-02-26 09:10:21 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# twitter-ovikello
+
+Using a usb web cam and a raspberry pi, 
+takes a photo when the button connected to GPIO pin 18 is pressed 
+and uploads the photo to Twitter.
+
+Requires a python file named 'auth.py' with valid Twitter keys and tokens to work.
---
+++ twitter_vahti.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+from twython import Twython
+import pygame.image
+import pygame.camera
+import time
+import RPi.GPIO as GPIO 
+
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+    )
+
+twitter = Twython (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+    )
+
+
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+cam = None
+
+
+
+while True:
+    try:
+        if cam == None:
+            pygame.camera.init()
+            cam = pygame.camera.Camera(pygame.camera.list_cameras()[0],(1280,720))
+            time.sleep(10)
+            cam.start()
+            print("kamera initialized")
+    
+
+            
+        
+            
+        if GPIO.input(18) == False:
+            
+            img = cam.get_image()
+            #time.sleep(2)
+            
+
+            pygame.image.save(img, "photo.jpg")
+            time.sleep(1)
+            #pygame.camera.quit()
+
+
+            message = " "
+            image = open('photo.jpg','rb')
+            response = twitter.upload_media(media=image)
+            media_id = [response['media_id']]
+
+            twitter.update_status(status=message, media_ids=media_id)
+            print("kuva otettu")
+            time.sleep(3)
+            print("camera off cooldown")
+            
+            
+    except:
+        if not(cam == None):
+            #pygame.camera.quit()
+            cam = None
+            print("kamera ei toiminut")
+            
